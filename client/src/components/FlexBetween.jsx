110c0d8d93bb3ed9d65b0eaff659d5215941a5eb
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-17 15:56:02 -0300

    FlexBetween Style Component

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import { style } from "@mui/system";
+
+const FlexBetween = style(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
