84dabe14bd9ba6fdefb3820e14ca259a777716a0
VinicinBraga <vinicius@bragamatta.com.br>
2023-02-17 15:30:38 -0300

    state

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./scenes/loginPage";
+import ProfilePage from "./scenes/profilePage";
+import HomePage from "./scenes/homePage/index";
+
 function App() {
-  return <div className="App">Helooooo</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
